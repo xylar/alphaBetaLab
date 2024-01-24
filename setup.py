b839512c6a66ab4ad7da3d9cd311cea2ba6c465f
Xylar Asay-Davis <xylarstorm@gmail.com>
2024-01-24 18:07:19 +0100

    Add back dependencies based on wiki

--- setup.py
+++ setup.py
@@ -6,5 +6,14 @@ setup(
     author='Lorenzo Mentaschi',
     author_email='lorenzo.mentaschi@unige.it',
     packages=['alphaBetaLab', 'alphaBetaLab.plot'],
+    install_requires=[
+        "cartopy >=0.21.1",
+        "fiona >=1.9.1",
+        "numpy >=1.8.2",
+        "matplotlib >=3.5.3",
+        "netcdf4 >=1.6.0",
+        "pandas >=1.4.3 ",
+        "scipy >=1.9.1",
+        "shapely >=2.0.1,<3.0"
+    ],
 )
- 
